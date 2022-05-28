6d98f157c24b764f3a5926cdb9e4335c5290c3f8
SWETHA N <91358401+SwethaN125@users.noreply.github.com>
2022-05-28 19:58:28 +0530

    Add files via upload

---
+++ Webpage-GoodBooks/app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/booksDB", {
+  useNewUrlParser: true
+})
+
+var Rec_Books_Genre = [];
+const booksSchema = {
+  title: String,
+  author: String,
+  genre: String,
+  rating: Number,
+  image: String
+};
+
+const Book = mongoose.model("Book", booksSchema);
+var Book_Found = {};
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.post("/", function(req, res) {
+
+  const book1 = req.body.Book_searched;
+  if (book1 != null) {
+    Book.findOne({
+      title: book1
+    }, function(err, found) {
+      if (found) {
+        Rec_Books_Genre.push(found.genre);
+        Book.find({
+          genre: Rec_Books_Genre
+        }, function(err, find) {
+          if (find.title != found.title) {
+            Book_Found = find;
+          }
+        }).limit(5);
+        res.render("book", {
+          title: found.title,
+          author: found.author,
+          genre: found.genre,
+          rating: found.rating,
+          image: found.image,
+          books: Book_Found
+        });
+      } else {
+        res.render("home");
+      }
+    });
+  }
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
